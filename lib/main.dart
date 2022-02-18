202d6e6dfbb631a468bed17b1fb61a52d100c842
Aurélien JACOB <jacob-aurelien@hotmail.fr>
2022-02-18 15:27:24 +0100

    tutorial 5 init

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,226 @@
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_1/data/model/album_model.dart';
+import 'package:flutter_layout_1/data/service/album_service.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'Returning Data',
-      home: HomeScreen(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Crud',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+  final TextEditingController _controller = TextEditingController();
+  late Future<Album>? _futureAlbum;
+  GlobalKey bottomNavigationKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    _futureAlbum = fetchAlbum();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Returning Data Demo'),
-      ),
-      body: const Center(
-        child: SelectionButton(),
-      ),
-    );
-  }
-}
-
-class SelectionButton extends StatelessWidget {
-  const SelectionButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: const Text('Pick an option, any option!'),
-    );
-  }
-
-  // A method that launches the SelectionScreen and awaits the result from
-  // Navigator.pop.
-  void _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SelectionScreen()),
-    );
-
-    // After the Selection Screen returns a result, hide any previous snackbars
-    // and show the new result.
-    ScaffoldMessenger.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text('$result')));
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
-  const SelectionScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick an option'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Yep!" as the result.
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: const Text('Yep!'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Nope." as the result.
-                  Navigator.pop(context, 'Nope.');
-                },
-                child: const Text('Nope.'),
-              ),
-            )
+        appBar: AppBar(
+          title: const Text("Flutter CRUD"),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(color: Colors.white),
+          child: Center(
+            child: _getPage(currentPage),
+          ),
+        ),
+        bottomNavigationBar: FancyBottomNavigation(
+          tabs: [
+            TabData(
+                iconData: Icons.home,
+                title: "Get Album",
+                onclick: () {
+                  final FancyBottomNavigationState fState = bottomNavigationKey
+                      .currentState as FancyBottomNavigationState;
+                  fState.setPage(0);
+                }),
+            TabData(
+                iconData: Icons.add,
+                title: "Add Album",
+                onclick: () {
+                  final FancyBottomNavigationState fState = bottomNavigationKey
+                      .currentState as FancyBottomNavigationState;
+                  fState.setPage(1);
+                }),
+            TabData(
+                iconData: Icons.update,
+                title: "Update Album",
+                onclick: () {
+                  final FancyBottomNavigationState fState = bottomNavigationKey
+                      .currentState as FancyBottomNavigationState;
+                  fState.setPage(2);
+                }),
+            TabData(
+                iconData: Icons.delete,
+                title: "Delete Album",
+                onclick: () {
+                  final FancyBottomNavigationState fState = bottomNavigationKey
+                      .currentState as FancyBottomNavigationState;
+                  fState.setPage(3);
+                }),
           ],
+          initialSelection: 0,
+          key: bottomNavigationKey,
+          onTabChangedListener: (position) {
+            setState(() {
+              currentPage = position;
+            });
+          },
+        ));
+  }
+
+  _getPage(int page) {
+    switch (page) {
+      case 0:
+        return getAlbumSection();
+      case 1:
+        return addAlbumSection();
+      case 2:
+        return updateAlbumSection();
+      case 3:
+        return deleteAlbumSection();
+      default:
+        return const Center(
+          child: Text("Nothing to display, sorry !"),
+        );
+    }
+  }
+
+  FutureBuilder<Album> getAlbumSection() {
+    return FutureBuilder<Album>(
+      future: _futureAlbum,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data?.title ?? 'Album deleted');
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+        // By default, show a loading spinner.
+        return const CircularProgressIndicator();
+      },
+    );
+  }
+
+  FutureBuilder<Album> deleteAlbumSection() {
+    return FutureBuilder<Album>(
+      future: _futureAlbum,
+      builder: (context, snapshot) {
+        // If the connection is done,
+        // check for response data or an error.
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(snapshot.data?.title ?? 'Album deleted'),
+                ElevatedButton(
+                  child: const Text('Delete album'),
+                  onPressed: () {
+                    setState(() {
+                      _futureAlbum = deleteAlbum(snapshot.data!.id.toString());
+                    });
+                  },
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+        }
+
+        // By default, show a loading spinner.
+        return const CircularProgressIndicator();
+      },
+    );
+  }
+
+  Padding addAlbumSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 40.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(hintText: 'Enter Title'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _futureAlbum = createAlbum(_controller.text);
+              });
+            },
+            child: const Text('Create album'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding updateAlbumSection() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder<Album>(
+          future: _futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(snapshot.data?.title ?? 'Enter new title'),
+                    TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Title',
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _futureAlbum = updateAlbum(_controller.text);
+                        });
+                      },
+                      child: const Text('Update Data'),
+                    ),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+            }
+
+            return const CircularProgressIndicator();
+          },
         ),
       ),
     );
